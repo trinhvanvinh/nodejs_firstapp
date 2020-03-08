01713150f5c5569490779eac3642bba297cb2fb3
Trịnh Văn Vinh <trinhvanvinh018@gmail.com>
2020-03-08 19:58:50 +0700

    add web

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require('http');
+// var http = require('http');
+//
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+//make way for some custom css, js and images
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
+var server = app.listen(8080, function(){
+  var port = server.address().port;
+  console.log("Server started at http://localhost:%s", port);
+});
+
